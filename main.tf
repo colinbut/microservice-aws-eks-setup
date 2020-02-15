48e10953381fca50b23e06d2b8bafd8cbafd5af7
Colin But <colin.but@outlook.com>
2020-02-15 23:48:28 +0000

    fixing terraform syntax issue for latest terraform version

--- main.tf
+++ main.tf
@@ -21,27 +21,26 @@ resource "aws_iam_role" "aws_eks_role_to_assume" {
 }
 
 resource "aws_iam_role_policy_attachment" "microservice-aws-eks-cluster-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam:aws:policy/AmazonEKSClusterPolicy"
-  role = "${aws_iam_role.aws_eks_role_to_assume}"
+    policy_arn = "arn:aws:iam:aws:policy/AmazonEKSClusterPolicy"
+    role = "aws_iam_role.aws_eks_role_to_assume"
 }
 
 resource "aws_iam_role_policy_attachment" "microservice-aws-eks-cluster-AmazonEKSServicePolicy" {
     policy_arn = "arn:aws:iam:aws:policy/AmazonEKSServicePolicy"
-    role = "${aws_iam_role.aws_eks_role_to_assume}"
+    role = "aws_iam_role.aws_eks_role_to_assume"
 }
-
 
 
 resource "aws_eks_cluster" "microservice-aws-eks-cluster" {
     name = "microservice-aws-eks-cluster"
-    role_arn = "${aws_iam_role.aws_eks_role_to_assume.arn}"
+    role_arn = aws_iam_role.aws_eks_role_to_assume.arn
 
     vpc_config { // default VPC of the region for now
         subnet_ids = ["subnet-89e691e0", "subnet-535eed29", "subnet-beb071f2"]
     }
 
     depends_on = [
-        "aws_iam_role_policy_attachment.microservice-aws-eks-cluster-AmazonEKSClusterPolicy",
-        "aws_iam_role_policy_attachment.microservice-aws-eks-cluster-AmazonEKSServicePolicy"
+        aws_iam_role_policy_attachment.microservice-aws-eks-cluster-AmazonEKSClusterPolicy,
+        aws_iam_role_policy_attachment.microservice-aws-eks-cluster-AmazonEKSServicePolicy
     ]
 }
